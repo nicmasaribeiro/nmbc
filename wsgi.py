80138fbb87e1f1e6de20e9b93345a059c5c4d8b6
nicmasaribeiro <46984245+nicmasaribeiro@users.noreply.github.com>
2025-03-18 14:55:07 -0300

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,16 @@
+from app import app
+from models import *
+import schedule 
 
+if __name__ == '__main__':
+	with app.app_context():
+		db.create_all()
+		PendingTransactionDatabase.genisis() 
+	def run_scheduler():
+		while True:
+			with app.app_context():
+				schedule.run_pending()
+				time.sleep(1)  #
+	schedule_thread = threading.Thread(target=run_scheduler, daemon=True)
+	schedule_thread.start()
+	app.run(host="0.0.0.0",port=8080)
