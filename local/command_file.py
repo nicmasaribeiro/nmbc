3e506bf69728b8bd5662ab088852471a7a3b03dc
nicmasaribeiro <46984245+nicmasaribeiro@users.noreply.github.com>
2025-03-19 23:47:45 -0300

    Add files via upload

--- local/command_file.py
+++ local/command_file.py
@@ -1,42 +1,35 @@
 #!/usr/bin/env python3
 from scipy.misc import derivative
-import scipy as sp
 import numpy as np
-from scipy.integrate import quad, dblquad
+from scipy.integrate import tplquad
+import scipy.integrate as spi
+
+b0 = 2.3
+b1 = -.34
+b2 =.4
+a = .3
+
+f = lambda t,s,a: ( b0 + b1*(( 1 - np.exp(-a * t)) /(a*t) )+ b2 * ((( 1 - np.exp(-a * t)) / (a*t) ) - np.exp(-a*t)))**s
+print(f(1,2,4))
 
 
-dt = 1/52
+# Define the integration limits
+def h1(x, y): return 0       # Lower limit for z
+def h2(x, y): return 1   # Upper limit for z
+	
+def g1(x): return 0          # Lower limit for y
+def g2(x): return 1      # Upper limit for y
+	
+#a, b = 0, 1   Limits for x
 
-ri,di,si = .03,.04,10
-rk,dk,sk = .033,.054,10
+# Compute the integral
+Ft = lambda t: spi.tplquad(f, 0, t, g1, g2, h1, h2)[0]
+print("Triple Integral Result:", Ft(2))
 
-pi = lambda x:sp.stats.poisson(dt).cdf(x)
-Bi = lambda x: si * (x - np.exp(-ri*x))
-fi = lambda t,T: np.exp((T-t)*(ri-di)) * Bi(T)
-ii = lambda x,y: fi(x,y)*pi(x)
-ui = lambda t,T: dblquad(ii,t,T,lambda x: t, lambda x: T)[0]
-print(ui(0,3))
+dfdt = lambda t: (a * np.exp(-a*t) * t - 1 + np.exp(-a*t))/(a*t**2)
+print("dfdt\t",dfdt(-1))
 
-pk = lambda x: sp.stats.norm(0,dt).pdf(x)
-Bk = lambda x: sk * (x - np.exp(-rk*x))
-fk = lambda t, T: np.exp((T-t)*(rk-dk)) * Bk(T)
-ik = lambda x,y: fk(x,y)*pk(x)
-uk = lambda t,T: dblquad(ik,t,T,lambda x: t, lambda x: T)[0]
-print(uk(0,3))
-
-p0 = lambda x,y: (1/np.cosh(x))*np.tanh(y)
-i_p0 =  lambda t,T : dblquad(p0, t, T, lambda x: t, lambda x: T)[0]
-Bik = lambda t,T: uk(t,T) + ui(t,T) + i_p0(t,T)
-print(Bik(0,2))
-
-i1 = lambda x,y: p0(x,y)*Bi(x)*Bk(y)
-i2 = quad(Bk, 0, 1)[0]
-i3 = quad(Bi, 0, 1)[0]
-Cik = lambda t,T : dblquad(i1, t	, T,lambda x: t, lambda x: T)[0]**2 - i2 * i3
-print(Cik(0,1))
-
-#
-#def dCikdt(t,T):
-#	V_func = lambda t,T : Cik(t,T)
+#def d1(t,s,a):
+#	V_func = lambda t,s,a: f(t,s,a)
 #	return derivative(V_func, t, dx=1e-5)
-#print(dCikdt(1, 2))
+#print(d1(1,1,1))
