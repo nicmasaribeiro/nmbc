57853b1a41ff11971a775d4d6584e1e823878b12
nicmasaribeiro <46984245+nicmasaribeiro@users.noreply.github.com>
2025-05-02 12:51:20 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -132,16 +132,16 @@ network.create_genesis_block()
 node_bc = NodeBlockchain()
 # PORT = random.randint(5000,6000)
 
-# app.config['CELERY_BROKER_URL'] = 'redis://red-cv8uqftumphs738vdlb0:6379'
-# app.config['CELERY_RESULT_BACKEND'] = 'redis://red-cv8uqftumphs738vdlb0:6379' 
+app.config['CELERY_BROKER_URL'] = 'redis://red-cv8uqftumphs738vdlb0:6379'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://red-cv8uqftumphs738vdlb0:6379' 
 
 app.register_blueprint(kaggle_bp, url_prefix="/app")
 app.register_blueprint(sequential_bp, url_prefix="/seq")
 
 register_template_filters(app)
-# # 
-app.config['CELERY_BROKER_URL'] = 'redis://localhçost:6379/0'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+# # # 
+# app.config['CELERY_BROKER_URL'] = 'redis://localhçost:6379/0'
+# app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
 celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(result_backend=app.config['CELERY_RESULT_BACKEND'])
